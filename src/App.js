d2357642a56351d2dc87906bc637d49389e9471a
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-11-29 08:27:00 -0600

    added welcome screen

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { extractLocations, getEvents } from './api';
+import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import './nprogress.css';
 import { InfoAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
     numberOfEvents: 32,
-    selectedLocation: 'all'
+    selectedLocation: 'all',
+    showWelcomeScreen: undefined,
   };
 
   updateEvents = (location, eventCount) => {
@@ -32,19 +34,21 @@ class App extends Component {
     });
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get('code');
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({
-          events
-          // : events.slice(0, this.state.numberOfEvents)
-          , 
-          locations: extractLocations(events)
-        });
+        this.setState({ events,locations: extractLocations(events) });
       }
     });
   }
+}
 
   componentWillUnmount() {
     this.mounted = false;
@@ -52,6 +56,7 @@ class App extends Component {
 
   render() {
     // const { numberOfEvents } = this.state;
+    if (this.state.showWelcomeScreen === undefined) return <div className='App' />;
     return (
       <div className='App'>
         {!navigator.onLine && (
@@ -65,6 +70,11 @@ class App extends Component {
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents}
         updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+          />
       </div>
     );
   }
