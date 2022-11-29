c6e519bfcc4ad475ad02a2fc84c9032e2cdb8ba4
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-11-29 05:15:15 -0600

    Added InfoAlert and ErrorAlert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
-    textWarning: '',
+    errorText: '',
+    events: []
   };
 
   handleInputChanged = (event) => {
     const value = event.target.value;
-    const warning = value < 1 ? 'Please enter a number greater than 0' : '';
-    this.setState({
-      numberOfEvents: value,
-      textWarning: warning,
-    });
+    if (value < 1 || value > 32) { 
+        this.setState({
+        numberOfEvents: value,
+        errorText: 'Enter a number from 1 to 32.'
+        });
+    } else {
+        this.setState({
+            numberOfEvents: event.target.value,
+            errorText: '',
+        });
+    }
 
     this.props.updateEvents(undefined, value);
   };
@@ -21,14 +29,15 @@ class NumberOfEvents extends Component {
     return (
       <div>
         <div className='number-of-events'>
-          <label>
-            Number of Events:
-            <input
-              type='number'
-              className='number-of-events-input'
-              min='1'
-              value={this.state.numberOfEvents}
-              onChange={this.handleInputChanged}
+            <ErrorAlert text={this.state.errorText} />
+            <label>
+                Number of Events:
+                <input
+                type='number'
+                className='number-of-events-input'
+                min='1'
+                value={this.state.numberOfEvents}
+                onChange={this.handleInputChanged}
             />
           </label>
         </div>
