7f542087d14901d44eb2279337b4aeaeb4714d2d
cjhart34 <104225229+cjhart34@users.noreply.github.com>
2022-11-29 18:48:36 -0600

    added data charts

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import './nprogress.css';
 import { InfoAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import { ScatterChart, Scatter, XAxis, YAxis, ResponsiveContainer, Legend, CartesianGrid, Tooltip } from 'recharts';
+import EventGenre from './EventGenre';
 
 class App extends Component {
   state = {
@@ -14,7 +16,7 @@ class App extends Component {
     locations: [],
     numberOfEvents: 32,
     selectedLocation: 'all',
-    showWelcomeScreen: undefined,
+    showWelcomeScreen: undefined
   };
 
   updateEvents = (location, eventCount) => {
@@ -54,8 +56,18 @@ class App extends Component {
     this.mounted = false;
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  }
+
   render() {
-    // const { numberOfEvents } = this.state;
+    const { numberOfEvents, events, locations } = this.state;
     if (this.state.showWelcomeScreen === undefined) return <div className='App' />;
     return (
       <div className='App'>
@@ -68,7 +80,24 @@ class App extends Component {
         <h4>Choose your nearest city</h4>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents}
-        updateEvents={this.updateEvents} />
+          updateEvents={this.updateEvents} />
+        <EventGenre events={events} />
+        <ResponsiveContainer height={400} >
+          <ScatterChart
+            width={400}
+            height={400}
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis type="number" dataKey="number" allowDecimals={false} name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+            <Legend verticalAlign='bottom' height={60} />
+          </ScatterChart>
+        </ResponsiveContainer>
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => {
